dbf9ca97a358b83441233f5e356ff0b25742b3c9
Your Name <Matthewjdemeo@knights.ucf.edu>
2019-07-08 17:57:04 -0400

    challenge build

--- Roll a Ball/Assets/Scripts/PlayerController.cs
+++ Roll a Ball/Assets/Scripts/PlayerController.cs
@@ -1,59 +1,84 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UI;
-
-public class PlayerController : MonoBehaviour{
-    public float  speed;
-    public Text countText;
-    public Text winText;
-
-    private Rigidbody rb;
-    private int count;
-
-    void Start ()
-    {
-        rb = GetComponent<Rigidbody>();
-        count = 0;
-        SetCountText ();
-        winText.text = ""; 
-    }
-
-    void Update ()
+﻿ using UnityEngine;
+ using UnityEngine.UI;
+ using System.Collections;
+ 
+ public class PlayerController : MonoBehaviour
+ {
+ 
+     public float speed;
+     public Text countText;
+     public Text winText;
+     public Text scoreText;
+     public Text livesText;
+     public Text loseText;
+     private Rigidbody rb;
+     private int count;
+     private int score; 
+     private int lives;
+ 
+     void Start()
+     {
+         rb = GetComponent<Rigidbody>();
+         count = 0;
+         score = 0;
+         lives = 3;
+         winText.text = "";
+         scoreText.text = "";
+         countText.text = "";
+         livesText.text = "";
+         loseText.text = "";
+ 
+         SetAllText();   
+     }
+ 
+void Update ()
     {
         if (Input.GetKey("escape"))
      Application.Quit();
     }
 
-    void FixedUpdate ()
-    {
-        float moveHorizontal = Input.GetAxis ("Horizontal"); 
-        float moveVertical = Input.GetAxis ("Vertical");  
+     void FixedUpdate()
+     {
+         float moveHorizontal = Input.GetAxis("Horizontal");
+         float moveVertical = Input.GetAxis("Vertical");
+ 
+         Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+ 
+         rb.AddForce(movement * speed);
+     }
+ 
+     void OnTriggerEnter(Collider other)
+     {
+         if (other.gameObject.CompareTag("Pick Up"))
+         {
+             other.gameObject.SetActive(false);
+             count++; 
+             score++; 
+             SetAllText();
+         }
+ 
+         else if (other.gameObject.CompareTag("Enemy"))
+         {
+             other.gameObject.SetActive(false);
+             score--; 
+             lives--;
+             SetAllText();
+         }
+     }
 
-        Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
-
-        rb.AddForce (movement * speed);
-    }
-
-
-        void OnTriggerEnter(Collider other)
-        {
-            if (other.gameObject.CompareTag("Pick Up"))
-            {
-                other.gameObject.SetActive (false);
-                count = count + 1;
-                SetCountText ();  
-            }
-        } 
-
-
-
-    void SetCountText ()
-    {
-        countText.text = "Count: " + count.ToString (); 
-        if (count >= 13)
-        {  
-            winText.text = "You Win!";
-        }
-    }
-}
+     void SetAllText()
+     {
+         scoreText.text = "Score: " + score.ToString(); 
+         livesText.text = "Lives: " + lives.ToString();
+         countText.text = "Count: " + count.ToString();
+         if (score >= 12)
+         {
+             winText.text = "You Win!";
+         }
+         if (lives == 0)
+         {
+             loseText.text = "You Lose!";
+         }
+ 
+     }
+ }
